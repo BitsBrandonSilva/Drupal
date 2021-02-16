e7111b0b96502696865d970b6a9a33f04b36fb38
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2021-02-16 12:53:26 -0500

    Servicio en Drupal

---
+++ Capacitacion/web/modules/forcontu_hello/src/Services/ForcontuHelloTools.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\forcontu_hello\Services;
+
+class ForcontuHelloTools {
+
+  /**
+   * Retorna 5 veces un texto
+   */
+    public function firstFunction() {
+      return str_repeat('Nicholas', 5);
+    }
+
+
+}
